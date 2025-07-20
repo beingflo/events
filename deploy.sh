631a409ca606bff5a94d95d76980c2a48ddfe1c8
Florian Marending <florian@marending.dev>
2025-07-20 18:58:53 +0200

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+die() { echo "$*" 1>&2 ; exit 1; }
+
+echo -e "Deploying events to production!"
+
+[ -z "$(git status --porcelain)" ] || die "There are uncommitted changes"
+
+docker --context arm compose --file compose.prod.yml pull || die "Failed to pull new image"
+docker --context arm compose --file compose.prod.yml up -d || die "Failed to bring compose up"
