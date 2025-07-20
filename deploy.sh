7e019e2238f7204f1b7db031b0c0d7719b96f897
Florian Marending <florian@marending.dev>
2025-07-20 20:06:03 +0200

    Try to fix credentials situation

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,7 @@ echo -e "Deploying events to production!"
 [ -z "$(git status --porcelain)" ] || die "There are uncommitted changes"
 
 docker --context arm compose --file compose.prod.yml pull || die "Failed to pull new image"
-docker --context arm compose --file compose.prod.yml up -d || die "Failed to bring compose up"
+
+# Pass .env.prod such that docker substitutes the clickhouse credentials in the inline config
+# This is NOT using the env_file directive on the otel-collector service
+docker --context arm compose --env-file .env.prod --file compose.prod.yml up -d || die "Failed to bring compose up"
