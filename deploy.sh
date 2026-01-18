7faba4cb9d876b4354f6b189aff3726d09ad8965
Florian Marending <florian@marending.dev>
2026-01-18 17:19:36 +0100

    Add encrypted .env.prod file

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,10 @@ echo -e "Deploying events to production!"
 
 [ -z "$(git status --porcelain)" ] || die "There are uncommitted changes"
 
+trap "rm -f .env.prod" EXIT
+export SOPS_AGE_KEY=$(op item get "SOPS age key - events" --reveal --fields "private key") || die "Failed to get age key from 1Password"
+sops -d --input-type dotenv --output-type dotenv .env.prod.enc > .env.prod || die "Failed to decrypt .env.prod file"
+
 docker --context arm compose --file compose.prod.yml pull || die "Failed to pull new image"
 
 # Pass .env.prod such that docker substitutes the clickhouse credentials in the inline config
